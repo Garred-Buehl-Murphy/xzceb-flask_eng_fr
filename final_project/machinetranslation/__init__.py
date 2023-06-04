631d367c20a0ac52257fb6650617b52862038f2e
Garred-Buehl-Murphy <garredmurphywork@gmail.com>
2023-06-04 12:56:21 -0400

    Final changes

--- final_project/machinetranslation/__init__.py
+++ final_project/machinetranslation/__init__.py
@@ -1,2 +1,3 @@
 from . import translator
-from . import tests
+##from tests import tests
+##I commented out tests because due to it being placed in its own directory, it may not run correctly depending on where it is run from.
